80c80e1ae358168d4e012f89c12562d9ebde3665
Enis Ay <enisay35@outlook.com>
2025-05-05 21:11:35 +0300

    ...

--- Assets/Scripts/ScriptsEnis/Bullet.cs
+++ Assets/Scripts/ScriptsEnis/Bullet.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float whiteDuration = 0.1f;
+    public GameObject hitEffectPrefab; // Inspector'dan atayacağın efekt prefabı
+    public float effectLifetime = 1f;   // Efektin kaç saniye sonra yok olacağı
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -14,7 +16,16 @@ public class Bullet : MonoBehaviour
                 enemyFlash.FlashWhite(whiteDuration);
             }
 
-            Destroy(gameObject); // Bullet yok olur ama enemy coroutine çalıştırmaya devam eder
+            // Vuruş efektini oluştur
+            /*
+            if (hitEffectPrefab != null)
+            {
+                GameObject effect = Instantiate(hitEffectPrefab, transform.position, Quaternion.identity);
+                Destroy(effect, effectLifetime); // Efekti bir süre sonra yok et
+            }*/
+
+            Destroy(gameObject); // Mermiyi yok et
         }
+        
     }
 }
